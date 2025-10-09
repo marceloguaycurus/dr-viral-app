1f2090f45010264c3284e53b18e4b03a2119a0b9
marceloguaycurus <marcelo.guaycurus@gmail.com>
2025-10-09 16:00:34 -0300

    aaa

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    // Fail build on TypeScript errors
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    // Fail build on ESLint errors
-    ignoreDuringBuilds: false,
-  },
+  typescript: { ignoreBuildErrors: false },
+  eslint: { ignoreDuringBuilds: false },
 };
 
 export default nextConfig;
