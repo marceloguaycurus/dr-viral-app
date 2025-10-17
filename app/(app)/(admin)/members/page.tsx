7415cfb32b270d5fc7dfbcff281073b846428c89
marceloguaycurus <marcelo.guaycurus@gmail.com>
2025-10-17 15:14:08 -0300

    members page

--- app/(app)/(admin)/members/page.tsx
+++ app/(app)/(admin)/members/page.tsx
@@ -1,16 +1,10 @@
-import { getMembers } from "@/lib/utils/dataFunctions/bd-management";
 import { getUserData } from "@/lib/utils/dataFunctions/bd-management";
-import { OrganizationMember } from "@prisma/client";
 
 export default async function MembersPage() {
   const userData = await getUserData();
   const companyId = userData?.activeCompanyId;
   if (!companyId) {
     return <div>Nenhuma empresa selecionada</div>;
-  }
-  const members: OrganizationMember[] = await getMembers(companyId);
-  if (!members) {
-    return <div>Nenhum membro encontrado</div>;
   }
   return (
     <div className="p-6">
